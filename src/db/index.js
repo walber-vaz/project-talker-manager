c2dc87b4f9408b471cfd7e747bf946c32821118b
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-04-15 18:35:50 -0300

    fix: erro not in await db connection

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,9 @@
 const connection = require('./connection');
 
-const getAll = async () => connection.execute('SELECT * FROM talkers');
+const getAll = async () => {
+  const data = await connection.execute('SELECT * FROM talkers;');
+  return data;
+}
 
 module.exports = {
   getAll,
