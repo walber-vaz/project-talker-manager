d0080442e7c7f359d1103562ed7fe044c67f4ad3
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-04-15 18:18:28 -0300

    feat: get db

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
 
-// não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
