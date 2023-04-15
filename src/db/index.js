9b2e1dfb10b7a53cd5217e4f33ae655411581a4a
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-04-15 18:37:13 -0300

    fix: erro missin semicolon

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ const connection = require('./connection');
 const getAll = async () => {
   const data = await connection.execute('SELECT * FROM talkers;');
   return data;
-}
+};
 
 module.exports = {
   getAll,
